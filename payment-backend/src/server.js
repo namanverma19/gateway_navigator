f7272ce8457e81034505e5ff69c676e6cb5fc1d8
NAMAN VERMA <b240406@skit.ac.in>
2026-04-20 13:54:18 +0530

    Fix CORS configuration in server.js

--- payment-backend/src/server.js
+++ payment-backend/src/server.js
@@ -22,7 +22,7 @@ const app = express();
 // 2. Global Middleware
 // CORS ko update kiya hai taaki frontend se credentials aur headers allow ho sakein
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  origin: process.env.FRONTEND_URL ;
   credentials: true
 })); 
 
